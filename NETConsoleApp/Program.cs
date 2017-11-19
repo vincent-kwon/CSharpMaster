c262c191db59c42c2f754d0f31677ba708038ffd
hobum kwon <hobum.kwon@gmail.com>
2017-11-19 09:45:15 +0900

    Initial version

---
+++ NETConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello");
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
